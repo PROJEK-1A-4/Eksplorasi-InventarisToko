8e89e88c1a3d47ff7e9c8b5962a01f426d843701
Richard <richardfadhilah120@gmail.com>
2026-02-23 18:03:56 +0700

    Add alias to import package, set condition param value, loop for checking data, and removal system

--- hapus_barang.py
+++ hapus_barang.py
@@ -1,5 +1,15 @@
-from database import data_inventaris
+from database import data_inventaris as data # rename agar lebih pendek
 
 def hapus_barang(id):
-    # TODO: cari dan hapus barang by id dari data_inventaris
-    pass
+    barang_ditemukan = None # default calue
+
+    for barang in data:
+        if barang["id"] == id:
+            barang_ditemukan = barang # Id barang
+            break
+
+    if barang_ditemukan: # none = false,
+        data.remove(barang_ditemukan) # mengahpus barang
+        print(f"Barang dengan ID {id} berhasil dihapus.") # message success
+    else:
+        print(f"Barang dengan ID {id} tidak ditemukan.") # message failed
