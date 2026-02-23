718381116f0605e56dfa3ab2a893c475b2ee06db
Kyla Khansa <kyla.khansa.tif425@polban.ac.id>
2026-02-23 21:49:09 +0700

    membuat fitur hapus barang

--- hapus_barang.py
+++ hapus_barang.py
@@ -1,5 +1,11 @@
 from database import data_inventaris
 
 def hapus_barang(id):
-    # TODO: cari dan hapus barang by id dari data_inventaris
-    pass
+    id_barang = input("Masukan ID barang yang ingin dihapus: ")
+    
+    for barang in data_inventaris:
+        if barang["id"] == id_barang:
+            data_inventaris.remove(barang)
+            print("Barang Berhasil dihapus!")
+            return 
+    print("Barang tidak ditemukan!") 
