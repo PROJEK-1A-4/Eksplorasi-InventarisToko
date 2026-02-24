4ec877612046fe22e17f7d3cd04984b12ac9af2a
Aulia Rahmi <aulia.rahmi.tif425@polban.ac.id>
2026-02-24 17:26:51 +0700

    commit kedua

--- update_barang.py
+++ update_barang.py
@@ -4,4 +4,11 @@ from database import data_inventaris def update_barang(id, nama_baru=None, jumla
     # Looping untuk mencari barang berdasarkan ID for barang in data_inventaris: 
     if barang['id'] == id: 
     # Jika nama_baru diberikan, update field 'nama' 
-    if nama_baru is not None: barang['nama'] = nama_baru
+    if nama_baru is not None: 
+        barang['nama'] = nama_baru
+    # Jika jumlah_baru diberikan, update field 'jumlah' 
+    if jumlah_baru is not None: 
+        barang['jumlah'] = jumlah_baru 
+    # Jika harga_baru diberikan, update field 'harga' 
+    if harga_baru is not None: 
+        barang['harga'] = harga_baru
